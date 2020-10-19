e8eb65fa0b641a7862651ce08670f0fe820d3e41
aditi <aditi@DESKTOP-343N0FS>
2020-10-19 22:10:08 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan10/logika/IF110119016Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan10.logika;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan logika
+ */
+public class IF110119016Latihan10Logika {
+    
+    public static void main(String[] args) {
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa );
+    }
+    
+}
